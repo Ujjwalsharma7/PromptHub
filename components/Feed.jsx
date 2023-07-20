193a19aa69001a06d87417abb0571cdc3f728095
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-20 23:13:05 +0530

    added search bar in feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,10 +1,23 @@
+"use client"
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const Feed = () => {
+ const [searchText, setSearchText] = useState('');
+ const handleSearchChange = (e) => {}
   return (
-    <div>
-      Feed
-    </div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input 
+        type='text'
+        placeholder='Search for a tag or a username'
+        value={searchText}
+        onChange={handleSearchChange}
+        required
+        className='search_input peer'
+        />
+      </form>
+    </section>
   )
 }
 
